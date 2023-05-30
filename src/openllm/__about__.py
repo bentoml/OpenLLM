e83621805346367e39025c2f386fe7e3cf03fecd
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-05-30 12:00:16 -0700

    infra: bump to dev version of 0.0.12.dev0 [generated]
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- src/openllm/__about__.py
+++ src/openllm/__about__.py
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "0.0.11"
+__version__ = "0.0.12.dev0"
