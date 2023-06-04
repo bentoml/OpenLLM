4990cf40c2621ed6e7a805c9bd579798571dba09
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-06-03 22:56:00 -0700

    chore(ci): update automatic release note
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

---
+++ .github/actions/create_release_and_archive.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Set by GH actions, see
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+TAG=${GITHUB_REF_NAME#v}
+PREFIX="openllm-${TAG}"
+ARCHIVE="openllm-${TAG}.tar.gz"
+
+git archive --format=tar --prefix="${PREFIX}/" "${TAG}" | gzip > "${ARCHIVE}"
+cat > release_notes.txt << EOF
+## Installation
+
+\`\`\`bash
+pip install openllm==${TAG}
+\`\`\`
+
+To upgrade from a previous version, use the following command:
+\`\`\`bash
+pip install --upgrade openllm==${TAG}
+\`\`\`
+
+## Usage
+
+All available models: \`\`\`python -m openllm.models\`\`\`
+
+To start a LLM: \`\`\`python -m openllm start dolly-v2\`\`\`
+
+EOF
