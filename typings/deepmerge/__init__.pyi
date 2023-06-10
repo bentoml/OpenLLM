d70530cb0ec4b467274c3be704f4d76e9ca7a3ee
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-06-10 03:04:56 -0400

    chore: add stubs for deepmerge [generated]
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

---
+++ typings/deepmerge/__init__.pyi
@@ -0,0 +1,13 @@
+from typing import List
+from typing import Tuple
+from typing import Union
+from typing import Literal
+
+from .merger import Merger
+
+DEFAULT_TYPE_SPECIFIC_MERGE_STRATEGIES: List[
+    Tuple[type, Union[Literal["append"], Literal["merge"], Literal["union"]]]
+] = ...
+always_merger: Merger = ...
+merge_or_raise: Merger = ...
+conservative_merger: Merger = ...
---
+++ typings/deepmerge/merger.pyi
@@ -0,0 +1,25 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Union
+
+from .strategy.set import SetStrategies
+from .strategy.core import StrategyList
+from .strategy.dict import DictStrategies
+from .strategy.list import ListStrategies
+
+ConfigDictType = Dict[str, Any]
+
+class Merger:
+    PROVIDED_TYPE_STRATEGIES: Dict[type, Union[ListStrategies, DictStrategies, SetStrategies]] = ...
+
+    def __init__(
+        self,
+        type_strategies: List[Tuple[type, str]],
+        fallback_strategies: List[str],
+        type_conflict_strategies: List[str],
+    ) -> None: ...
+    def merge(self, base: ConfigDictType, nxt: ConfigDictType) -> None: ...
+    def type_conflict_strategy(self, *args: Any) -> Any: ...
+    def value_strategy(self, path: str, base: StrategyList, nxt: StrategyList) -> None: ...
