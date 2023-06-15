a686bc750f458e6e4228158bd3a6804ea80a9b40
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-06-15 06:12:02 -0400

    style: run hooks and tools
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- examples/langchain-chains-demo/service.py
+++ examples/langchain-chains-demo/service.py
@@ -1,3 +1,17 @@
+# Copyright 2023 BentoML Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Any, Dict
 
 import bentoml
@@ -16,8 +30,8 @@ class Query(BaseModel):
 
 
 llm = OpenLLM(
-    model_name='dolly-v2',
-    model_id='databricks/dolly-v2-7b',
+    model_name="dolly-v2",
+    model_id="databricks/dolly-v2-7b",
     embedded=False,
 )
 prompt = PromptTemplate(
@@ -25,7 +39,7 @@ prompt = PromptTemplate(
     template="""
 You are a Facebook Ads Copywriter with a strong background in persuasive
 writing and marketing. You craft compelling copy that appeals to the target
-audience's emotions and needs, peruading them to take action or make a 
+audience's emotions and needs, peruading them to take action or make a
 purchase. You are given the following context to create a facebook ad copy.
 It should provide an attention-grabbing headline optimizied for capivating
 leads and perusaive calls to action.
@@ -43,23 +57,13 @@ svc = bentoml.Service("fb-ads-copy", runners=[llm.runner])
 SAMPLE_INPUT = Query(
     industry="SAAS",
     product_name="BentoML",
-    keywords=[
-        "open source",
-        "developer tool",
-        "AI application platform",
-        "serverless",
-        "cost-efficient"
-    ],
-    llm_config=llm.runner.config.model_dump()
+    keywords=["open source", "developer tool", "AI application platform", "serverless", "cost-efficient"],
+    llm_config=llm.runner.config.model_dump(),
 )
 
-@svc.api(
-    input=JSON.from_sample(sample=SAMPLE_INPUT),
-    output=Text()
-)
+
+@svc.api(input=JSON.from_sample(sample=SAMPLE_INPUT), output=Text())
 def generate(query: Query):
-    return chain.run({
-        'industry': query.industry,
-        'product_name': query.product_name,
-        'keywords': ", ".join(query.keywords)
-    })
+    return chain.run(
+        {"industry": query.industry, "product_name": query.product_name, "keywords": ", ".join(query.keywords)}
+    )
