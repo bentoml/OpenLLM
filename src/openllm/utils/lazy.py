233d4697b57a88d6e6660d63982184c00356392e
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-06-16 18:13:23 -0400

    chore: update __all__ to take into _extra_objects
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- src/openllm/utils/lazy.py
+++ src/openllm/utils/lazy.py
@@ -44,15 +44,18 @@ class LazyModule(types.ModuleType):
         super().__init__(name)
         self._modules = set(import_structure.keys())
         self._class_to_module: dict[str, str] = {}
+        _extra_objects = {} if extra_objects is None else extra_objects
         for key, values in import_structure.items():
             for value in values:
                 self._class_to_module[value] = key
         # Needed for autocompletion in an IDE
-        self.__all__ = list(import_structure.keys()) + list(itertools.chain(*import_structure.values()))
+        self.__all__ = (
+            list(import_structure.keys()) + list(itertools.chain(*import_structure.values())) + list(_extra_objects)
+        )
         self.__file__ = module_file
         self.__spec__ = module_spec
         self.__path__ = [os.path.dirname(module_file)]
-        self._objects = {} if extra_objects is None else extra_objects
+        self._objects = _extra_objects
         self._name = name
         self._import_structure = import_structure
 
