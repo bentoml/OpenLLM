0be33da778b0026bde2daac51b99df7fb3bc684b
aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>
2023-06-24 09:21:17 +0000

    chore: update examples
    Signed-off-by: aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>

--- examples/bentoml-demo/service.py
+++ examples/bentoml-demo/service.py
@@ -26,7 +26,12 @@ llm_runner = openllm.Runner(model, llm_config=llm_config)
 svc = bentoml.Service(name="llm-service", runners=[llm_runner])
 
 
+@svc.on_startup
+def download(_: bentoml.Context):
+    llm_runner.llm.ensure_model_id_exists()
+
+
 @svc.api(input=bentoml.io.Text(), output=bentoml.io.Text())
 async def prompt(input_text: str) -> str:
     answer = await llm_runner.generate.async_run(input_text)
-    return answer
+    return answer[0]["generated_text"]
