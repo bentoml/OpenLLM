1d521a0112e4774abdd7100a12a1f8c726ab966e
GutZuFusss <leon.ikinger@googlemail.com>
2023-07-13 14:12:32 +0200

    Add models data (currently available ids + types)

---
+++ dashboard/clojure/src/main/openllm/components/model_selection/data.cljs
@@ -0,0 +1,40 @@
+(ns openllm.components.model-selection.data)
+
+(def models
+  {"chatglm" {:ids ["thudm/chatglm-6b"
+                    "thudm/chatglm-6b-int8"
+                    "thudm/chatglm-6b-int4"
+                    "thudm/chatglm2-6b"
+                    "thudm/chatglm2-6b-int4"]}
+   "dolly-v2" {:ids ["databricks/dolly-v2-3b"
+                     "databricks/dolly-v2-7b"
+                     "databricks/dolly-v2-12b"]}
+   "falcon" {:ids ["tiiuae/falcon-7b"
+                   "tiiuae/falcon-40b"
+                   "tiiuae/falcon-7b-instruct"
+                   "tiiuae/falcon-40b-instruct"]}
+   "flan-t5" {:ids ["google/flan-t5-small"
+                    "google/flan-t5-base"
+                    "google/flan-t5-large"
+                    "google/flan-t5-xl"
+                    "google/flan-t5-xxl"]}
+   "gpt-neox" {:ids ["eleutherai/gpt-neox-20b"]}
+   "mpt" {:ids ["mosaicml/mpt-7b"
+                "mosaicml/mpt-7b-instruct"
+                "mosaicml/mpt-7b-chat"
+                "mosaicml/mpt-7b-storywriter"
+                "mosaicml/mpt-30b"
+                "mosaicml/mpt-30b-instruct"
+                "mosaicml/mpt-30b-chat"]}
+   "opt" {:ids ["facebook/opt-125m"
+                "facebook/opt-350m"
+                "facebook/opt-1.3b"
+                "facebook/opt-2.7b"
+                "facebook/opt-6.7b"
+                "facebook/opt-66b"]}
+   "stablelm" {:ids ["stabilityai/stablelm-tuned-alpha-3b"
+                     "stabilityai/stablelm-tuned-alpha-7b"
+                     "stabilityai/stablelm-base-alpha-3b"
+                     "stabilityai/stablelm-base-alpha-7b"]}
+   "starcoder" {:ids ["bigcode/starcoder"
+                      "bigcode/starcoderbase"]}})
