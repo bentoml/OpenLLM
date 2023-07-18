6632e82b91d349a0ad4aaad97fe2c64a86843ed3
GutZuFusss <leon.ikinger@googlemail.com>
2023-07-18 23:14:45 +0200

    Add clojure spec for model data
    If there are changes in the backend, we want things to break!

--- contrib/clojure-ui/src/main/openllm/components/model_selection/data.cljs
+++ contrib/clojure-ui/src/main/openllm/components/model_selection/data.cljs
@@ -1,10 +1,37 @@
 (ns openllm.components.model-selection.data
-  (:require-macros [openllm.build :refer [slurp]]))
+  (:require-macros [openllm.build :refer [slurp]])
+  (:require [clojure.spec.alpha :as s]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                Spec                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(s/def ::vec-of-runtimes? (s/coll-of
+                           (s/and string?
+                                  #(or (= % "pt")
+                                       (= % "flax")  ;; currently available runtimes
+                                       (= % "tf")))
+                           :kind vector?))
+
+(s/def ::model_id (s/coll-of string? :kind vector?)) ;; model_id is a vector of all models for a given model_type
+(s/def ::url string?)                                ;; url to the model's page
+(s/def ::requires_gpu boolean?)                      ;; whether the model requires a GPU
+(s/def ::runtime_impl ::vec-of-runtimes?)            ;; supported runtimes
+(s/def ::installation string?)                       ;; installation instructions (pip command)
+
+(s/def ::model-spec
+  (s/keys :req-un [::model_id ::url ::requires_gpu   ;; the spec for a single model (aggregates all the above)
+                   ::runtime_impl ::installation]))
+
+(s/def ::models (s/map-of keyword? ::model-spec))    ;; map of all models
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;             Slurp Data             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def models-data (-> "./src/generated/models-data.json"
                      (slurp ,) ;; see `openllm.build/slurp` to see how this magic works
                      (js/JSON.parse ,)
-                     (js->clj ,)))
+                     (js->clj , :keywordize-keys true)))
 
 (def models
   {"chatglm" {:ids ["thudm/chatglm-6b"
