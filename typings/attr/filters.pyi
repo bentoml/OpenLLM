c0ec7d1fd83b780f04876055596309518a9187f3
aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>
2023-07-18 17:43:25 +0000

    chore(stub): remove attr stubs
    inferred from source
    
    Signed-off-by: aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>

--- typings/attr/filters.pyi
+++
@@ -1,8 +0,0 @@
-from typing import Any
-from typing import Union
-
-from . import Attribute
-from . import _FilterType
-
-def include(*what: Union[type, str, Attribute[Any]]) -> _FilterType[Any]: ...
-def exclude(*what: Union[type, str, Attribute[Any]]) -> _FilterType[Any]: ...
