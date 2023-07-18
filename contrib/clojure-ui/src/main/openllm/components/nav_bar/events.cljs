e3f14d44f98916ca362b137ff31459f0a6eead38
GutZuFusss <leon.ikinger@googlemail.com>
2023-07-18 14:15:38 +0200

    Clean up building of export contents

--- contrib/clojure-ui/src/main/openllm/components/nav_bar/events.cljs
+++ contrib/clojure-ui/src/main/openllm/components/nav_bar/events.cljs
@@ -3,7 +3,10 @@
             [clojure.string :as str]
             [re-frame.core :refer [reg-event-fx]]))
 
-(defn start-download!
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;              Functions             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- start-download!
   "Starts the download of a file."
   [file-name content]
   (let [blob (js/Blob. #js [content] #js {:type "text/plain"})
@@ -13,20 +16,43 @@
     (set! (.-target link) "_blank")
     (.click link)))
 
+(defn- build-playground-export-contents
+  "Returns the contents of the playground export file."
+  [prompt response]
+  (str "Prompt: " prompt
+       "\n\n\n"
+       "Response: " response))
+
+(defn- build-chat-export-contents
+  "Returns the contents of the chat export file."
+  [chat-history]
+  (let [mapping-fn (fn [current-entry]
+                     (str/join ": "
+                               [(name (:user current-entry))
+                                (:text current-entry)]))]
+    (str/join "\n"
+              (map mapping-fn
+                   chat-history))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               Events               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (reg-event-fx
  ::export-button-clicked
  (fn [cofx _]
    {:dispatch-sync true
-    :fx (let [textareas (js/document.querySelectorAll "textarea")
-              active-screen (get-in cofx [:db :screen-id])]
+    :fx (let [active-screen (get-in cofx [:db :screen-id])]
           (condp = active-screen
             :playground
+            (let [input-value (get-in cofx [:db :playground-input-value])
+                  response-value (get-in cofx [:db :playground-last-response])]
             (start-download! "export-playground.txt"
-                             (str "Prompt: " (str/join "\n\n\nResponse: "
-                                                       (map #(.-value %) textareas))))
+                             (build-playground-export-contents input-value
+                                                               response-value)))
             :chat
-            (start-download! "export-chat.txt"
-                             (str/join "\n" (map #(str/join ": " [(name (:user %)) (:text %)])
-                                                 (get-in cofx [:db :chat-history]))))
+            (let [chat-history (get-in cofx [:db :chat-history])]
+              (start-download! "export-chat.txt"
+                               (build-chat-export-contents chat-history)))
             ;default
             (log :error "Export button clicked in unknown screen.")))}))
