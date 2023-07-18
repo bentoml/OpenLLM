d4e2379e3e2c66e1a20fdd888c5c45b06e1f8b87
aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>
2023-07-18 19:54:30 +0000

    revert: "chore(stub): remove attr stubs"
    This reverts commit c0ec7d1fd83b780f04876055596309518a9187f3.

---
+++ typings/attr/_cmp.pyi
@@ -0,0 +1,15 @@
+from typing import Any
+from typing import Callable
+from typing import Optional
+
+_CompareWithType = Callable[[Any, Any], bool]
+
+def cmp_using(
+    eq: Optional[_CompareWithType] = ...,
+    lt: Optional[_CompareWithType] = ...,
+    le: Optional[_CompareWithType] = ...,
+    gt: Optional[_CompareWithType] = ...,
+    ge: Optional[_CompareWithType] = ...,
+    require_same_type: bool = ...,
+    class_name: str = ...,
+) -> type[Any]: ...
---
+++ typings/attr/setters.pyi
@@ -0,0 +1,17 @@
+from typing import Any
+from typing import NewType
+from typing import NoReturn
+from typing import TypeVar
+
+from . import Attribute
+from . import _OnSetAttrType
+
+_T = TypeVar("_T")
+
+def frozen(instance: Any, attribute: Attribute[Any], new_value: Any) -> NoReturn: ...
+def pipe(*setters: _OnSetAttrType) -> _OnSetAttrType: ...
+def validate(instance: Any, attribute: Attribute[_T], new_value: _T) -> _T: ...
+def convert(instance: Any, attribute: Attribute[Any], new_value: Any) -> Any: ...
+
+_NoOpType = NewType("_NoOpType", object)
+NO_OP: _NoOpType
