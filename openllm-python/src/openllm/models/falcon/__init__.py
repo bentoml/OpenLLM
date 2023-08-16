e7934b96cd7f0ababb7685bc219fbd2b6dbf016b
aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>
2023-08-16 09:47:10 +0000

    feat(models): add vLLM support for Falcon
    Signed-off-by: aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>

--- openllm-python/src/openllm/models/falcon/__init__.py
+++ openllm-python/src/openllm/models/falcon/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import sys, typing as t
 from openllm.exceptions import MissingDependencyError
-from openllm.utils import LazyModule, is_torch_available
+from openllm.utils import LazyModule, is_torch_available, is_vllm_available
 
 _import_structure: dict[str, list[str]] = {"configuration_falcon": ["FalconConfig", "START_FALCON_COMMAND_DOCSTRING", "DEFAULT_PROMPT_TEMPLATE"]}
 if t.TYPE_CHECKING:
@@ -16,5 +16,11 @@ except MissingDependencyError: pass
 else:
   _import_structure["modeling_falcon"] = ["Falcon"]
   if t.TYPE_CHECKING: from .modeling_falcon import Falcon as Falcon
+try:
+  if not is_vllm_available(): raise MissingDependencyError
+except MissingDependencyError: pass
+else:
+  _import_structure["modeling_vllm_falcon"] = ["VLLMFalcon"]
+  if t.TYPE_CHECKING: from .modeling_vllm_falcon import VLLMFalcon as VLLMFalcon
 
 sys.modules[__name__] = LazyModule(__name__, globals()["__file__"], _import_structure)
