d6a1ef2f1c4c467590d6361d74520569ef6d40fb
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-09-18 06:44:39 +0000

    infra: bump to homebrew tap release to 0.3.5 [generated] [skip ci]
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- Formula/openllm.rb
+++ Formula/openllm.rb
@@ -4,37 +4,37 @@
 class Openllm < Formula
   desc "OpenLLM: Operating LLMs in production"
   homepage "https://github.com/bentoml/OpenLLM"
-  version "0.3.4"
+  version "0.3.5"
   license "Apache-2.0"
   head "https://github.com/bentoml/OpenLLM, branch: main"
-  url "https://github.com/bentoml/OpenLLM/archive/v0.3.4.tar.gz"
-  sha256 "0a1e82d50c448803d9d34de760f4a2636184dc12d27238c67bbaf993194815ce"
+  url "https://github.com/bentoml/OpenLLM/archive/v0.3.5.tar.gz"
+  sha256 "3d334d8415c4d74fb4f52e7a987c87e00c3a0ba91c03d111ab6843823ad646d1"
 
   on_linux do
-    url "https://github.com/bentoml/OpenLLM/releases/download/v0.3.4/openllm-0.3.4-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "fd133cd8845c62f82e7e2a4638fcb62040efd10436be2cf948e22a18168dccd2"
+    url "https://github.com/bentoml/OpenLLM/releases/download/v0.3.5/openllm-0.3.5-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "323449804adb95249ba41289eda483a08e6306dc5d77bd6c742a47787686e189"
   end
   on_macos do
     on_arm do
-      url "https://github.com/bentoml/OpenLLM/releases/download/v0.3.4/openllm-0.3.4-aarch64-apple-darwin.tar.gz"
-      sha256 "151ed629a721b56d2d206ac39a64f51bbe4025def27db7c0dac9ad6258cb3294"
+      url "https://github.com/bentoml/OpenLLM/releases/download/v0.3.5/openllm-0.3.5-aarch64-apple-darwin.tar.gz"
+      sha256 "c4364336159fca11008987335145d8299f4d8c11d436e0650512495e4c3360a6"
     end
     on_intel do
-      url "https://github.com/bentoml/OpenLLM/releases/download/v0.3.4/openllm-0.3.4-x86_64-apple-darwin.tar.gz"
-      sha256 "58053064b981a4e1867c5569c4e4a78f8d29ec863fb164e2058d06550aa73164"
+      url "https://github.com/bentoml/OpenLLM/releases/download/v0.3.5/openllm-0.3.5-x86_64-apple-darwin.tar.gz"
+      sha256 "92f71dacb234df0adbe8de542d4847502f7d54f42fb930c19133f7920adf07a0"
     end
   end
 
   def install
     on_linux do
-      bin.install "openllm-0.3.4-x86_64-unknown-linux-musl" => "openllm"
+      bin.install "openllm-0.3.5-x86_64-unknown-linux-musl" => "openllm"
     end
   on_macos do
     on_arm do
-      bin.install "openllm-0.3.4-aarch64-apple-darwin" => "openllm"
+      bin.install "openllm-0.3.5-aarch64-apple-darwin" => "openllm"
     end
     on_intel do
-      bin.install "openllm-0.3.4-x86_64-apple-darwin" => "openllm"
+      bin.install "openllm-0.3.5-x86_64-apple-darwin" => "openllm"
     end
   end
     ohai "To get started, run: 'openllm --help'"
