31ca71354cf8a29d043a6b1c9f9bb7441831da12
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-10-07 00:23:12 -0400

    chore: update required attributes
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- openllm-python/src/openllm/_service.py
+++ openllm-python/src/openllm/_service.py
@@ -69,24 +69,24 @@ async def generate_stream_v1(input_dict: dict[str, t.Any]) -> t.AsyncGenerator[s
   else:
     return runner.generate_iterator.async_stream(qa_inputs.prompt, adapter_name=qa_inputs.adapter_name, echo=echo, **qa_inputs.llm_config.model_dump())
 
-_CompletionInput = bentoml.io.JSON.from_sample(openllm.utils.bentoml_cattr.unstructure(openllm.openai.CompletionRequest()))
-
-@svc.api(route='v1/completions', input=_CompletionInput, output=bentoml.io.Text())
+@svc.api(route='v1/completions',
+         input=bentoml.io.JSON.from_sample(openllm.utils.bentoml_cattr.unstructure(openllm.openai.CompletionRequest(prompt='What is 1+1?', model=runner.llm_type))),
+         output=bentoml.io.Text())
 async def completion_v1(input_dict: dict[str, t.Any], ctx: bentoml.Context) -> str | t.AsyncGenerator[str, None]:
-  prompt = input_dict.pop('prompt', '')
+  prompt = input_dict.pop('prompt', None)
+  if prompt is None: raise ValueError("'prompt' should not be None.")
   stream = input_dict.pop('stream', False)
   config = {
-    'max_new_tokens': input_dict.pop('max_tokens', llm_config['max_new_tokens']),
-    'temperature': input_dict.pop('temperature', llm_config['temperature']),
-    'top_p': input_dict.pop('top_p', llm_config['top_p']),
-    'n': input_dict.pop('n', llm_config['n']),
-    'logprobs': input_dict.pop('logprobs', llm_config['logprobs']),
-    'echo': input_dict.pop('echo', False),
-    'stop': input_dict.pop('stop', llm_config['stop']),
-    'presence_penalty': input_dict.pop('presence_penalty', llm_config['presence_penalty']),
-    'frequency_penalty': input_dict.pop('frequency_penalty', llm_config['frequency_penalty']),
-    'best_of': input_dict.pop('best_of', llm_config['best_of']),
-    'adapter_name': input_dict.pop('adapter_name', None)
+      'max_new_tokens': input_dict.pop('max_tokens', llm_config['max_new_tokens']),
+      'temperature': input_dict.pop('temperature', llm_config['temperature']),
+      'top_p': input_dict.pop('top_p', llm_config['top_p']),
+      'n': input_dict.pop('n', llm_config['n']),
+      'logprobs': input_dict.pop('logprobs', llm_config['logprobs']),
+      'echo': input_dict.pop('echo', False),
+      'stop': input_dict.pop('stop', llm_config['stop']),
+      'presence_penalty': input_dict.pop('presence_penalty', llm_config['presence_penalty']),
+      'frequency_penalty': input_dict.pop('frequency_penalty', llm_config['frequency_penalty']),
+      'best_of': input_dict.pop('best_of', llm_config['best_of']),
   }
 
   async def stream_response_generator(responses: t.AsyncGenerator[str, None]) -> t.AsyncGenerator[str, None]:
@@ -117,22 +117,27 @@ async def completion_v1(input_dict: dict[str, t.Any], ctx: bentoml.Context) -> s
                                               model=runner.llm_type)  # TODO: logprobs, finish_reason and usage
         )).decode()
 
-_ChatCompletionInput = bentoml.io.JSON.from_sample(openllm.utils.bentoml_cattr.unstructure(openllm.openai.ChatCompletionRequest()))
-
-@svc.api(route='/v1/chat/completions', input=_ChatCompletionInput, output=bentoml.io.Text())
+@svc.api(route='/v1/chat/completions',
+         input=bentoml.io.JSON.from_sample(
+             openllm.utils.bentoml_cattr.unstructure(
+                 openllm.openai.ChatCompletionRequest(messages=[{
+                     'role': 'system', 'content': 'You are a helpful assistant.'
+                 }, {
+                     'role': 'user', 'content': 'Hello!'
+                 }], model=runner.llm_type))),
+         output=bentoml.io.Text())
 async def chat_completion_v1(input_dict: dict[str, t.Any], ctx: bentoml.Context) -> str | t.AsyncGenerator[str, None]:
   prompt = openllm.openai.messages_to_prompt(input_dict['messages'])
   stream = input_dict.pop('stream', False)
   config = {
-    'temperature': input_dict.pop('temperature', llm_config['temperature']),
-    'top_p': input_dict.pop('top_p', llm_config['top_p']),
-    'n': input_dict.pop('n', llm_config['n']),
-    'echo': input_dict.pop('echo', False),
-    'stop': input_dict.pop('stop', llm_config['stop']),
-    'max_new_tokens': input_dict.pop('max_tokens', llm_config['max_new_tokens']),
-    'presence_penalty': input_dict.pop('presence_penalty', llm_config['presence_penalty']),
-    'frequency_penalty': input_dict.pop('frequency_penalty', llm_config['frequency_penalty']),
-    'adapter_name': input_dict.pop('adapter_name', None)
+      'temperature': input_dict.pop('temperature', llm_config['temperature']),
+      'top_p': input_dict.pop('top_p', llm_config['top_p']),
+      'n': input_dict.pop('n', llm_config['n']),
+      'echo': input_dict.pop('echo', False),
+      'stop': input_dict.pop('stop', llm_config['stop']),
+      'max_new_tokens': input_dict.pop('max_tokens', llm_config['max_new_tokens']),
+      'presence_penalty': input_dict.pop('presence_penalty', llm_config['presence_penalty']),
+      'frequency_penalty': input_dict.pop('frequency_penalty', llm_config['frequency_penalty']),
   }
 
   async def stream_response_generator(responses: t.AsyncGenerator[str, None]) -> t.AsyncGenerator[str, None]:
