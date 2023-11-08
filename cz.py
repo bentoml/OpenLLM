d4011b084ebae47649ee7413dc3e12f31c4fab25
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-11-08 03:10:57 +0000

    chore: update testing script
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- cz.py
+++ cz.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-from __future__ import annotations
-import itertools
-import os
-import token
-import tokenize
+import itertools, os, token, tokenize
 
 from tabulate import tabulate
 
@@ -14,7 +10,8 @@ def run_cz(dir: str, package: str):
   table = []
   for path, _, files in os.walk(os.path.join(dir, 'src', package)):
     for name in files:
-      if not name.endswith('.py'): continue
+      if not name.endswith('.py'):
+        continue
       filepath = os.path.join(path, name)
       with tokenize.open(filepath) as file_:
         tokens = [t for t in tokenize.generate_tokens(file_.readline) if t.type in TOKEN_WHITELIST]
@@ -23,9 +20,12 @@ def run_cz(dir: str, package: str):
   print(f'\n{"=" * 80}\n')
   print(tabulate([headers, *sorted(table, key=lambda x: -x[1])], headers='firstrow', floatfmt='.1f') + '\n')
   print(
-      tabulate([(dir_name, sum([x[1] for x in group])) for dir_name, group in itertools.groupby(sorted([(x[0].rsplit('/', 1)[0], x[1]) for x in table]), key=lambda x: x[0])],
-               headers=['Directory', 'LOC'],
-               floatfmt='.1f'))
+    tabulate(
+      [(dir_name, sum([x[1] for x in group])) for dir_name, group in itertools.groupby(sorted([(x[0].rsplit('/', 1)[0], x[1]) for x in table]), key=lambda x: x[0])],
+      headers=['Directory', 'LOC'],
+      floatfmt='.1f',
+    )
+  )
   print(f'total line count for {package}: {sum([x[1] for x in table])}\n')
 
 def main() -> int:
