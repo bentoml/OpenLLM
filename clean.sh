62d6f950f38f86b680874ce7d63075e35cde14f4
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-11-16 17:38:19 -0500

    infra: remove codegolf
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- clean.sh
+++ clean.sh
@@ -2,4 +2,5 @@
 GIT_ROOT="$(git rev-parse --show-toplevel)"
 cd "$GIT_ROOT" || exit 1
 find . -type f -iname "*.so" -exec rm -f {} \;
-find . -type d -iname "node_modules" -exec rm -rf "{}" \;
+find . -type d -iname "__pycache__" -exec rm -rf {} \; -prune
+find . -type d -iname "node_modules" -exec rm -rf "{}" \; -prune
--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-pip install -U mypy==1.6.1 build==0.10.0 hatchling==1.18.0 hatch-vcs==0.3.0 hatch-fancy-pypi-readme==23.1.0 hatch-mypyc==0.16.0
+pip install -U mypy==1.7.0 "build<1" hatchling==1.18.0 hatch-vcs==0.3.0 hatch-fancy-pypi-readme==23.1.0 hatch-mypyc==0.16.0
+HATCH_VERBOSE=3 MYPY_CONFIG_FILE_DIR="${SCRIPT_DIR}" HATCH_BUILD_HOOKS_ENABLE=1 python -m build openllm-client -w -C--global-option=--verbose "$@"
 HATCH_VERBOSE=3 MYPY_CONFIG_FILE_DIR="${SCRIPT_DIR}" HATCH_BUILD_HOOKS_ENABLE=1 python -m build openllm-core -w -C--global-option=--verbose "$@"
 HATCH_VERBOSE=3 MYPY_CONFIG_FILE_DIR="${SCRIPT_DIR}" HATCH_BUILD_HOOKS_ENABLE=1 python -m build openllm-python -w -C--global-option=--verbose "$@"
 hatch clean
