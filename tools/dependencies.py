c03e3bebb3bb13b1e000a6465e91cd001a30f936
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-11-17 16:02:25 -0500

    fix(infra): prepare correct dependencies for release [skip ci] (#687)
    fix(infra): prepare correct dependencies for release
    
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- tools/dependencies.py
+++ tools/dependencies.py
@@ -14,6 +14,7 @@ if t.TYPE_CHECKING:
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(ROOT, 'openllm-python', 'src'))
+sys.path.insert(1, os.path.join(ROOT, 'openllm-core', 'src'))
 
 import openllm
 
