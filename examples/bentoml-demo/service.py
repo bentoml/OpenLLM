2d81e2cc8451a5598c69261fe21309067f64e450
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-11-17 08:31:06 -0500

    fix(examples): add support for streaming feature
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- examples/bentoml-demo/service.py
+++ examples/bentoml-demo/service.py
@@ -1,14 +1,49 @@
-from __future__ import annotations
+import uuid
+from typing import Any, AsyncGenerator, Dict, TypedDict, Union
 
-import bentoml
-import openllm
+from bentoml import Service
+from bentoml.io import JSON, Text
+from openllm import LLM
 
-llm = openllm.LLM('facebook/opt-2.7b')
-
-svc = bentoml.Service(name='llm-service', runners=[llm.runner])
+llm = LLM[Any, Any]('HuggingFaceH4/zephyr-7b-beta', backend='vllm')
 
 
-@svc.api(input=bentoml.io.Text(), output=bentoml.io.Text())
-async def prompt(input_text: str) -> str:
-  generation = await llm.generate(input_text)
-  return generation.outputs[0].text
+svc = Service('tinyllm', runners=[llm.runner])
+
+
+class GenerateInput(TypedDict):
+  prompt: str
+  stream: bool
+  sampling_params: Dict[str, Any]
+
+
+@svc.api(
+  route='/v1/generate',
+  input=JSON.from_sample(
+    GenerateInput(prompt='What is time?', stream=False, sampling_params={'temperature': 0.73, 'logprobs': 1})
+  ),
+  output=Text(content_type='text/event-stream'),
+)
+async def generate(request: GenerateInput) -> Union[AsyncGenerator[str, None], str]:
+  n = request['sampling_params'].pop('n', 1)
+  request_id = f'tinyllm-{uuid.uuid4().hex}'
+  previous_texts = [''] * n
+
+  generator = llm.generate_iterator(request['prompt'], request_id=request_id, n=n, **request['sampling_params'])
+
+  async def streamer() -> AsyncGenerator[str, None]:
+    async for request_output in generator:
+      for output in request_output.outputs:
+        i = output.index
+        delta_text = output.text[len(previous_texts[i]) :]
+        previous_texts[i] = output.text
+        yield delta_text
+
+  if request['stream']:
+    return streamer()
+
+  final_output = None
+  async for request_output in generator:
+    final_output = request_output
+  assert final_output is not None
+  return final_output.outputs[0].text
