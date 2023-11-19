97654b8e78826239d6096e1da85dc540f981782c
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-11-18 21:44:25 -0500

    --wip--
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- openllm-core/src/openllm_core/_typing_compat.py
+++ openllm-core/src/openllm_core/_typing_compat.py
@@ -33,7 +33,7 @@ At = t.TypeVar('At', bound=attr.AttrsInstance)
 LiteralDtype = t.Literal['float16', 'float32', 'bfloat16', 'int8', 'int16']
 LiteralSerialisation = t.Literal['safetensors', 'legacy']
 LiteralQuantise = t.Literal['int8', 'int4', 'gptq', 'awq', 'squeezellm']
-LiteralBackend = t.Literal['pt', 'vllm', 'ggml', 'mlc']
+LiteralBackend = t.Literal['pt', 'vllm', 'ctranslate', 'ggml', 'mlc']
 AdapterType = t.Literal[
   'lora', 'adalora', 'adaption_prompt', 'prefix_tuning', 'p_tuning', 'prompt_tuning', 'ia3', 'loha', 'lokr'
 ]
