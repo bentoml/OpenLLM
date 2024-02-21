7edbcf8a2d6a49566018a7903c28368a03600fa4
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-21 04:27:40 -0500

    infra: migrate to uv
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- cz.py
+++ cz.py
@@ -22,13 +22,19 @@ def run_cz(args):
       with tokenize.open(filepath) as file_:
         tokens = [t for t in tokenize.generate_tokens(file_.readline) if t.type in TOKEN_WHITELIST]
         token_count, line_count = len(tokens), len(set([t.start[0] for t in tokens]))
-        table.append([filepath.replace(os.path.join(args.dir, 'src'), ''), line_count, token_count / line_count if line_count != 0 else 0])
+        table.append([
+          filepath.replace(os.path.join(args.dir, 'src'), ''),
+          line_count,
+          token_count / line_count if line_count != 0 else 0,
+        ])
   print(tabulate([headers, *sorted(table, key=lambda x: -x[1])], headers='firstrow', floatfmt='.1f') + '\n')
   print(
     tabulate(
       [
         (dir_name, sum([x[1] for x in group]))
-        for dir_name, group in itertools.groupby(sorted([(x[0].rsplit('/', 1)[0], x[1]) for x in table]), key=lambda x: x[0])
+        for dir_name, group in itertools.groupby(
+          sorted([(x[0].rsplit('/', 1)[0], x[1]) for x in table]), key=lambda x: x[0]
+        )
       ],
       headers=['Directory', 'LOC'],
       floatfmt='.1f',
@@ -46,6 +52,10 @@ if __name__ == '__main__':
 
   parser = argparse.ArgumentParser()
   parser.add_argument(
-    '--dir', choices=['openllm-python', 'openllm-core', 'openllm-client'], help='directory to check', default='openllm-python', required=False
+    '--dir',
+    choices=['openllm-python', 'openllm-core', 'openllm-client'],
+    help='directory to check',
+    default='openllm-python',
+    required=False,
   )
   raise SystemExit(run_cz(parser.parse_args()))
