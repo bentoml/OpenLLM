a3cc7544d8e4a10e30d1819904f4d18e103f53f5
bojiang <bojiang_@outlook.com>
2024-05-31 21:19:16 +0800

    add chattts

---
+++ chattts/service.py
@@ -0,0 +1,55 @@
+import os
+import yaml
+from typing import Annotated
+import sys
+import shutil
+import io
+
+import bentoml
+from bentoml.validators import ContentType
+from bento_constants import CONSTANT_YAML
+
+
+CONSTANTS = yaml.safe_load(CONSTANT_YAML)
+
+
+@bentoml.service(**CONSTANTS["service_config"])
+class Main:
+
+    @bentoml.on_deployment
+    @staticmethod
+    def on_deployment():
+        CHAT_TTS_REPO = os.environ.get("CHAT_TTS_REPO")
+        assert CHAT_TTS_REPO, "CHAT_TTS_REPO environment variable is not set"
+
+        import dulwich
+        import dulwich.errors
+        import dulwich.porcelain
+
+        if os.path.exists("ChatTTS"):
+            shutil.rmtree("ChatTTS")
+
+        dulwich.porcelain.clone(
+            CHAT_TTS_REPO,
+            "ChatTTS",
+            checkout=True,
+            depth=1,
+        )
+
+    def __init__(self) -> None:
+        sys.path.append(os.path.join(os.path.dirname(__file__), "ChatTTS"))
+
+        import ChatTTS
+
+        self.chat = ChatTTS.Chat()
+        self.chat.load_models(compile=False) # Set to True for better performance
+
+    @bentoml.api
+    def tts(self, text: str = "PUT YOUR TEXT HERE") -> Annotated[bytes, ContentType("audio/wav")]:
+        import torch
+        import torchaudio
+
+        wavs = self.chat.infer([text])
+        output_io = io.BytesIO()
+        torchaudio.save(output_io, torch.from_numpy(wavs[0]), 24000, format="wav")
+        return output_io.getvalue()
