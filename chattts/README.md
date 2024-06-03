47fefe30ed3513cafc454588dedd513d9b62d62c
bojiang <bojiang_@outlook.com>
2024-06-03 17:25:32 +0800

    chattts

--- chattts/README.md
+++ chattts/README.md
@@ -1,15 +1,41 @@
-## Local Run & development
+# BentoChatTTS
+
+[ChatTTS](https://github.com/2noise/ChatTTS) is a text-to-speech model designed specifically for dialogue scenario such as LLM assistant. 
+
+## Prerequisites
+
+- You have installed Python 3.9+ and `pip`. See the [Python downloads page](https://www.python.org/downloads/) to learn more.
+- You have a basic understanding of key concepts in BentoML, such as Services. We recommend you read [Quickstart](https://docs.bentoml.com/en/latest/get-started/quickstart.html) first.
+- (Optional) We recommend you create a virtual environment for dependency isolation for this project. See the [Conda documentation](https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html) or the [Python documentation](https://docs.python.org/3/library/venv.html) for details.
+
+## Install Dependencies
 
 ```
 pip install bentoml
 pip install -r requirements.txt
+```
 
+If not already present, you need to install `libsox-dev` with your package manager first.
+
+## Run
+
+Start a Bento server with ChatTTS.
+
+```
 export CHAT_TTS_REPO=https://github.com/2noise/ChatTTS.git
 bentoml serve
 ```
 
-(You may need to install `libsox-dev` with your package manager first)
+The server is now active at [http://localhost:3000](http://localhost:3000/). You can interact with it using the Swagger UI.
 
-## Get your own server
+## Deploy
 
-Get your token at: https://cloud.bentoml.com/
+You can deploy the ChatTTS Bento service to BentoCloud.
+
+[Sign up](https://www.bentoml.com/) if you haven't got a BentoCloud account.
+
+Make sure you have [logged in to BentoCloud](https://docs.bentoml.com/en/latest/bentocloud/how-tos/manage-access-token.html), then run the following command to deploy it.
+
+```bash
+bentoml deploy .
+```
--- chattts/service.py
+++ chattts/service.py
@@ -15,6 +15,7 @@ CONSTANTS = yaml.safe_load(CONSTANT_YAML)
 
 CHATTTS_PATH = os.path.join(os.path.dirname(__file__), "ChatTTS")
 
+
 @bentoml.service(**CONSTANTS["service_config"])
 class Main:
 
@@ -44,14 +45,47 @@ class Main:
         import ChatTTS
 
         self.chat = ChatTTS.Chat()
-        self.chat.load_models(compile=False) # Set to True for better performance
+        self.chat.load_models(compile=False)  # Set to True for better performance
 
     @bentoml.api
-    def tts(self, text: str = "PUT YOUR TEXT HERE") -> Annotated[pathlib.Path, ContentType("audio/wav")]:
+    def tts(
+        self,
+        text: str = "PUT YOUR TEXT HERE",
+        speaker: str = "2",
+    ) -> Annotated[pathlib.Path, ContentType("audio/wav")]:
+        rhythm: bool = True
+        temperature: float = 0.3
+        top_P: float = 0.7
+        top_K: int = 20
+
         import torch
         import torchaudio
 
-        wavs = self.chat.infer([text])
+        if speaker:
+            seed = int(speaker, 16)
+            torch.manual_seed(seed)
+
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed(seed)
+                torch.cuda.manual_seed_all(seed)
+
+        dim = self.chat.pretrain_models["gpt"].gpt.layers[0].mlp.gate_proj.in_features
+        std, mean = self.chat.pretrain_models["spk_stat"].chunk(2)
+        rand_spk = torch.randn(dim, device=std.device) * std + mean
+
+        params_infer_code = {
+            "spk_emb": rand_spk,
+            "temperature": temperature,
+            "top_P": top_P,
+            "top_K": top_K,
+        }
+
+        wavs = self.chat.infer(
+            [text],
+            skip_refine_text=rhythm,
+            params_infer_code=params_infer_code,
+        )
+
         output_io = io.BytesIO()
         torchaudio.save(output_io, torch.from_numpy(wavs[0]), 24000, format="wav")
         return output_io.getvalue()
