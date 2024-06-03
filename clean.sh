fe17a235ce4c4579ccf842931343cfaff6e3c232
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2024-06-03 17:09:18 -0400

    chore(ci): prepare for releases
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-GIT_ROOT="$(git rev-parse --show-toplevel)"
-cd "$GIT_ROOT" || exit 1
-find . -type f -iname "*.so" -exec rm -f {} \;
-find . -type d -iname "__pycache__" -exec rm -rf {} \; -prune
-find . -type d -iname "node_modules" -exec rm -rf "{}" \; -prune
