ca306adde70541cfa524cb2b3a4fa7cef7d1c214
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2024-06-11 05:44:32 +0000

    infra: update installation deps
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- all.sh
+++ all.sh
@@ -3,10 +3,6 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd "$GIT_ROOT" || exit 1
 
-if command -v direnv >/dev/null 2>&1 && [ -f "$GIT_ROOT/.envrc" ]; then
-  direnv allow
-fi
-
 printf "Running update-mypy.py\n"
 python ./tools/update-mypy.py
 printf "Running update-config-stubs.py\n"
