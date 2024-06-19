b1ad075795ed17c10d268a796219903247d00f57
Rick Zhou <rickzhoucmu@gmail.com>
2024-06-19 09:07:07 +0000

    fix: update chaat UI

--- vllm-chat/ui/static/4iQRzhUxuyYGI-qRRxANF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-afe7495db7bcec52.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
